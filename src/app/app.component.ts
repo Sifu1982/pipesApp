545555bba078e0ef7911e8feda6660a001ac1df1
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-09-23 09:13:07 +0200

    feat: created menu with PrimeNg and router.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'daVid marTínez';
+  valor: number = 1000;
+  obj = {
+    nombre: 'David',
+  };
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
